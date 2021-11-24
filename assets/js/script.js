9611c49f800152c2f5476ccf792caac92539777e
Matt Grogan <mattgrogan.dev@gmail.com>
2021-11-23 17:32:07 -0700

    updated javascript and now functioning properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,37 @@ var determineTime = moment().hour();
 
 console.log(determineTime);
 
-var textArea = document.getElementsByClassName("text-area");
-var saveBtn = document.getElementsByClassName("saveBtn");
+var textArea = document.querySelector(".text-area");
+var saveBtn = document.querySelector(".container");
 
-console.log(textArea);
-console.log(saveBtn);
+saveBtn.addEventListener("click", function (event) {
+  if (event.target.nodeName === "BUTTON") {
+    console.log(event.target.attributes[0].value);
+    let inputId = event.target.attributes[0].value;
+    let inputValue = document.getElementById(inputId).value;
+    localStorage.setItem(inputId, inputValue);
+  }
+});
+
+for (let i = 0; i < 9; i++) {
+  document.getElementById("userText" + i).value = localStorage.getItem(
+    "userText" + i
+  );
+  console.log(document.getElementById("userText" + i).attributes[0].value);
+  if (
+    parseInt(document.getElementById("userText" + i).attributes[2].value) <
+    determineTime
+  ) {
+    document.getElementById("userText" + i).classList.add("past");
+  } else if (
+    parseInt(document.getElementById("userText" + i).attributes[2].value) ===
+    determineTime
+  ) {
+    document.getElementById("userText" + i).classList.add("present");
+  } else if (
+    parseInt(document.getElementById("userText" + i).attributes[2].value) >
+    determineTime
+  ) {
+    document.getElementById("userText" + i).classList.add("future");
+  }
+}
